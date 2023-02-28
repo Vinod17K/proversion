8aaf77a755e6b61a7983eae865ce60c8f31184a1
vinodkumaree17@gmail.com <vinodkumaree17@gmail.com>
2023-02-28 08:16:52 +0530

    iam upload see it

--- StringProperties.java
+++ StringProperties.java
@@ -1,8 +1,13 @@
 package ObjectClass;
 
 public class StringProperties {
+	public String toString() {
+		return "hello";
+	}
 
 	public static void main(String[] args) {
+		StringProperties s = new StringProperties();
+		System.out.print(s);
 
 	}
 
